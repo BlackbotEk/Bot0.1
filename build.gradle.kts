928a62d80f03ea53012dd482e2970ff8411568df
BlackbotEk <castillofrias24@gmail.com>
2026-03-05 06:50:59 -0400

    uild.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    kotlin("android") version "1.9.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
